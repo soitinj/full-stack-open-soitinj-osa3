a74a187d07e5910a0b312f3e4aade07325419a74
Jaakko <jaakko.soitinaho@veikkaus.fi>
2024-04-17 09:50:01 +0300

    Serve static dist folder

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const app = express()
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :payload'))
 app.use(cors())
+app.use(express.static('dist'))
 
 let persons = [
   {
