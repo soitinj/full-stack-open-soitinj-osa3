0f5d0c686ad8538f21aa906a258a818d3b80b621
Jaakko <jaakko.soitinaho@veikkaus.fi>
2024-04-19 21:55:15 +0300

    3.13-14 submission. Fetch person data from mongodb

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person')
 
 morgan.token('payload', (req, res) => ( JSON.stringify(req.body) ))
 const app = express()
@@ -34,39 +36,59 @@ let persons = [
 
 app.get('/info', (request, response) => {
   const currentDate = Date()
-  response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${currentDate}</p>`)
+  Person.find({}).then(result => {
+    response.send(`<p>Phonebook has info for ${result.length} people</p><p>${currentDate}</p>`)
+  })
+  //response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${currentDate}</p>`)
 })
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then(result => {
+    response.json(result)
+  })
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  const person = persons.find(p => p.id === Number(request.params.id))
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }
+  //const person = persons.find(p => p.id === Number(request.params.id))
+  const person = Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      response.status(400).send({ error: 'Malformatted id' })
+    })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  persons = persons.filter(p => p.id !== Number(request.params.id))
-  response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => response.status(204).end())
+    .catch(error => next(error))
+  //persons = persons.filter(p => p.id !== Number(request.params.id))
+  //response.status(204).end()
 })
 
 app.post('/api/persons', (request, response) => {
   // For simplicity, no uuid is used
   const body = request.body
-  const randomId = Math.floor(Math.random() * 1000000)
-  const newPerson = {'id': randomId, 'name': body.name, 'number': body.number}
+  //const randomId = Math.floor(Math.random() * 1000000)
+  //const newPerson = {'id': randomId, 'name': body.name, 'number': body.number}
+  const newPerson = new Person({
+    name: body.name,
+    number: body.number
+  })
   if (!(newPerson.name && newPerson.number)) {
     response.status(400).json({'error': 'Name or number missing'})
-  } else if (persons.find(p => p.name.toLowerCase() === newPerson.name.toLowerCase())) {
-    response.status(409).json({'error': 'Name already exists'})
+  //} else if (persons.find(p => p.name.toLowerCase() === newPerson.name.toLowerCase())) {
+  //  response.status(409).json({'error': 'Name already exists'})
   } else {
-    persons = [...persons, newPerson]
-    response.json(newPerson)
+    newPerson.save().then(result => {
+      response.json(result)
+    })
   }
 })
 
